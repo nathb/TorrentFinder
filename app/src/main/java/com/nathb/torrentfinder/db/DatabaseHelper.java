784351b005217a30e366e90cca809a987739e92c
nathb <nath04@gmail.com>
2016-09-23 20:54:45 +1000

    Added new shows

--- app/src/main/java/com/nathb/torrentfinder/db/DatabaseHelper.java
+++ app/src/main/java/com/nathb/torrentfinder/db/DatabaseHelper.java
@@ -48,6 +48,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.insert(ShowDao.TABLE_NAME, null, generateShowContentValues("QI", "QI", "QI"));
         db.insert(ShowDao.TABLE_NAME, null, generateShowContentValues("Once Upon a Time", "Once Upon a Time", "OnceUponaTime"));
         db.insert(ShowDao.TABLE_NAME, null, generateShowContentValues("Archer", "Archer", "Archer"));
+        db.insert(ShowDao.TABLE_NAME, null, generateShowContentValues("House of Cards", "House of Cards", "HouseofCards"));
+        db.insert(ShowDao.TABLE_NAME, null, generateShowContentValues("Mr Robot", "Mr Robot", "MrRobot"));
     }
 
     private ContentValues generateShowContentValues(String title, String torrentSearchTerm, String episodeListSearchTerm) {
